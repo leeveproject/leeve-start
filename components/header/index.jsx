b5b70410e9a5a264e8aa7b511aff1d24f0bc0536
23eerick <erickwelberth1@hotmail.com>
2023-10-31 21:09:19 -0300

    feat: develop header feature

--- components/header/index.jsx
+++ components/header/index.jsx
@@ -1,11 +1,32 @@
+"use client"
+
 import Image from "next/image";
 import { NavBar } from "./nav";
 import { ButtonAcess } from "./button";
+import { useState, useEffect } from "react";
 import { Container, Box } from "@chakra-ui/react";
 
 export default function Header() {
+  const [isHeaderVisible, setIsHeaderVisible] = useState(true);
+
+  useEffect(() => {
+    function handleScroll() {
+      if (window.scrollY > 300) {
+        setIsHeaderVisible(false)
+      } else {
+        setIsHeaderVisible(true)
+      }
+    }
+
+    window.addEventListener("scroll", handleScroll)
+  })
+
+
   return (
-    <header id="header">
+    <header
+      id="header"
+      style={{ display: isHeaderVisible ? "block" : "none" }}
+    >
       <Container
         zIndex="1"
         maxWidth="100vw"
